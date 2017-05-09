0bffe9f470bf27ab30461a359148ba352b6d1365
RsuhanB <Rushanben@hotmail.com>
2017-05-09 18:19:35 -0400

    Final Commit - Working

---
+++ Stoyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stoyboard
+//
+//  Created by Rushan on 2017-05-09.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stoyboard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Stoyboard
+//
+//  Created by Rushan on 2017-05-09.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
